fa6db54357f2b23f17fb4b6ebdace2954473ff78
Isaac Njenga <ayzzoh20@gmail.com>
2025-01-27 10:42:16 +0300

    additional changes

---
+++ back/node_modules/back/node_modules/back/node_modules/back/node_modules/back/node_modules/back/node_modules/back/node_modules/back/node_modules/back/node_modules/back/node_modules/back/node_modules/back/node_modules/back/node_modules/back/node_modules/back/node_modules/back/node_modules/back/node_modules/back/node_modules/back/node_modules/back/node_modules/back/node_modules/back/controllers/productController.js
@@ -0,0 +1,7 @@
+const addProduct = (req, res) => {};
+const getProducts = (req, res) => {};
+const getProduct = (req, res) => {};
+const updateProduct = (req, res) => {};
+const deleteProduct = (req, res) => {};
+
+export { addProduct, getProducts, getProduct, updateProduct, deleteProduct };
--- back/node_modules/back/node_modules/back/node_modules/back/node_modules/back/node_modules/back/node_modules/back/node_modules/back/node_modules/back/node_modules/back/node_modules/back/node_modules/back/node_modules/back/node_modules/back/node_modules/back/node_modules/back/node_modules/back/node_modules/back/node_modules/back/node_modules/back/node_modules/back/node_modules/back/routes/routes.js
+++ back/node_modules/back/node_modules/back/node_modules/back/node_modules/back/node_modules/back/node_modules/back/node_modules/back/node_modules/back/node_modules/back/node_modules/back/node_modules/back/node_modules/back/node_modules/back/node_modules/back/node_modules/back/node_modules/back/node_modules/back/node_modules/back/node_modules/back/node_modules/back/node_modules/back/routes/routes.js
@@ -1,4 +1,19 @@
 import express from "express";
 const router = express.Router();
 
+import {
+  addProduct,
+  deleteProduct,
+  getProduct,
+  getProducts,
+  updateProduct,
+} from "../controllers/productController.js";
+
+//product endpoints
+router.post("/add-product", addProduct);
+router.get("/get-all-products", getProducts);
+router.get("/get-product/:id", getProduct);
+router.put("/update-product", updateProduct);
+router.delete("/delete-product", deleteProduct);
+
 export { router as Router };
